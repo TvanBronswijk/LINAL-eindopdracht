08932f3ea86dc0b135702e4f097d55f1c8065fdc
Tobi Bronswijk <294604@student.fontys.nl>
2018-11-05 13:24:14 +0100

    initial scaffolding with SDL2 dependency

---
+++ LINAL-eindopdracht/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <SDL.h>
+
+int main(int argc, char * argv[])
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
+	{
+		std::cout << "SDL initialization failed. SDL Error: " << SDL_GetError();
+	}
+	else
+	{
+		std::cout << "SDL initialization succeeded!";
+	}
+
+	std::cin.get();
+	return 0;
+}

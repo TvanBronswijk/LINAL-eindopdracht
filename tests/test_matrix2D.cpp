d57b0a9f82bebaa2edbd35e89ef2db7cd9dc0d2e
Rick van Berlo <rickvanberlo@gmail.com>
2019-01-10 19:41:41 +0100

    matrix 2d

---
+++ tests/test_matrix2D.cpp
@@ -0,0 +1,42 @@
+#include <engine/math/matrix2D.hpp>
+
+int make() {
+	//math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+	//	{ 1.0f, 1.0f, 1.0f },
+	//	{ 1.0f, 1.0f, 1.0f },
+	//	{ 1.0f, 1.0f, 1.0f }
+	//	} };
+
+	//if (matrix_1.at(1,1) != 1.0f || matrix_1.at(2,2) != 1.0f) {
+	//	return 0;
+	//}
+	//else {
+	//	return 1;
+	//}
+	return 1;
+}
+
+int main() {
+	int number = 0;
+	try {
+		number += make();
+		//number += plus();
+		//number += minus();
+		//number += matrix_times_scalar();
+		//number += scalar_times_matrix();
+		//number += matrix_times_matrix();
+		//number += divide_by_scalar();
+		//number += plus_is();
+		//number += minus_is();
+		//number += times_is();
+		//number += divide_by_is();
+		//number += matrix_operator();
+		//number += foreach();
+
+
+		return number;
+	}
+	catch (int e) {
+		return 1;
+	}
+}

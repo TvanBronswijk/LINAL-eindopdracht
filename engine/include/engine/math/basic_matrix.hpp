cfe36d5b70563f5722874b3bbb0b2e9175f9c9e9
Tobi Bronswijk <294604@student.fontys.nl>
2019-01-10 13:06:46 +0100

    updated matrix

--- engine/include/engine/math/basic_matrix.hpp
+++ engine/include/engine/math/basic_matrix.hpp
@@ -1,9 +1,11 @@
 #pragma once
 #include <cassert>
+#include <functional>
 #include <vector>
 #include <array>
 
 namespace math {
+	//TODO: multiply with vector
 	template<class T, size_t Rows>
 	struct umatrix {
 	public:
@@ -21,82 +23,69 @@ namespace math {
 		const T& at(size_t r, size_t c) const { return _collection[r * columns() + c]; }
 		T& operator () (size_t r, size_t c) { return at(r, c); }
 		const T& operator () (size_t r, size_t c) const { return at(r, c); }
+		void foreach(std::function<void(T&)> func) {
+			for (size_t ri = 0; ri < rows(); ri++)
+				for (size_t ci = 0; ci < columns(); ci++)
+					func(at(ri, ci));
+		}
+		void foreach(std::function<void(T&, size_t, size_t)> func) {
+			for (size_t ri = 0; ri < rows(); ri++)
+				for (size_t ci = 0; ci < columns(); ci++)
+					func(at(ri, ci), ri, ci);
+		}
 
 		umatrix<T, Rows>& operator += (const umatrix<T, Rows>& r) {
 			assert(columns() == r.columns());
-			for (size_t ri = 0; ri < rows(); ri++)
-				for (size_t ci = 0; ci < columns(); ci++)
-					at(ri, ci) += r(ri, ci);
+			foreach([&](auto& val, auto ri, auto ci) { val += r(ri, ci); });
 			return *this;
 		}
 		umatrix<T, Rows>& operator -= (const umatrix<T, Rows>& r) {
 			assert(columns() == r.columns());
-			for (size_t ri = 0; ri < rows(); ri++)
-				for (size_t ci = 0; ci < columns(); ci++)
-					at(ri, ci) -= r(ri, ci);
+			foreach([&](auto& val, auto ri, auto ci) { val -= r(ri, ci); });
 			return *this;
 		}
-		umatrix<T, Rows>& operator *= (const uscalar<T>& r) {
-			for (size_t ri = 0; ri < rows(); ri++)
-				for (size_t ci = 0; ci < columns(); ci++)
-					at(ri, ci) *= r;
-			return *this;
-		}
+		umatrix<T, Rows>& operator *= (uscalar<T> r) { foreach([&](auto& val) { val *= r; }); return *this; }
+		umatrix<T, Rows>& operator /= (uscalar<T> r) { foreach([&](auto& val) { val /= r; }); return *this; }
 	private:
 		size_t _rows;
 		size_t _columns;
 		std::vector<T> _collection;
 	};
 
-	template<class T, size_t Rows>
-	umatrix<T, Rows> operator + (const umatrix<T, Rows>& l, const umatrix<T, Rows>& r) {
+	template<class T, size_t Rows> umatrix<T, Rows> operator + (const umatrix<T, Rows>& l, const umatrix<T, Rows>& r) {
 		assert(l.columns() == r.columns());
 		umatrix<T, Rows> result(r.columns());
-		for (size_t ri = 0; ri < result.rows(); ri++)
-			for (size_t ci = 0; ci < result.columns(); ci++)
-				result(ri, ci) = l(ri, ci) + r(ri, ci);
+		result.foreach([&](auto& val, auto ri, auto ci) { val = l(ri, ci) + r(ri, ci); });
 		return result;
 	}
 
-	template<class T, size_t Rows>
-	umatrix<T, Rows> operator - (const umatrix<T, Rows>& l, const umatrix<T, Rows>& r) {
+	template<class T, size_t Rows> umatrix<T, Rows> operator - (const umatrix<T, Rows>& l, const umatrix<T, Rows>& r) {
 		assert(l.columns() == r.columns());
 		umatrix<T, Rows> result(r.columns());
-		for (size_t ri = 0; ri < result.rows(); ri++)
-			for (size_t ci = 0; ci < result.columns(); ci++)
-				result(ri, ci) = l(ri, ci) - r(ri, ci);
+		result.foreach([&](auto& val, auto ri, auto ci) { val = l(ri, ci) - r(ri, ci); });
 		return result;
 	}
 
-	template<class T, size_t Rows>
-	umatrix<T, Rows> operator * (const uscalar<T>& l, const umatrix<T, Rows>& r) {
+	template<class T, size_t Rows> umatrix<T, Rows> operator * (uscalar<T> l, const umatrix<T, Rows>& r) {
 		umatrix<T, Rows> result(r.columns());
-		for (size_t ri = 0; ri < result.rows(); ri++)
-			for (size_t ci = 0; ci < result.columns(); ci++)
-				result(ri, ci) = l * r(ri, ci);
+		result.foreach([&](auto& val, auto ri, auto ci) { val = l * r(ri, ci); });
 		return result;
 	}
 
-	template<class T, size_t Rows>
-	umatrix<T, Rows> operator * (const umatrix<T, Rows>& l, const uscalar<T>& r) {
+	template<class T, size_t Rows> umatrix<T, Rows> operator * (const umatrix<T, Rows>& l, uscalar<T> r) {
 		umatrix<T, Rows> result(l.columns());
-		for (size_t ri = 0; ri < result.rows(); ri++)
-			for (size_t ci = 0; ci < result.columns(); ci++)
-				result(ri, ci) = l(ri, ci) * r;
+		result.foreach([&](auto& val, auto ri, auto ci) { val = l(ri, ci) * r; });
 		return result;
 	}
 
-	template<class T, size_t Rows>
-	umatrix<T, Rows> operator * (const umatrix<T, Rows>& l, const umatrix<T, Rows>& r) {
+	template<class T, size_t Rows> umatrix<T, Rows> operator * (const umatrix<T, Rows>& l, const umatrix<T, Rows>& r) {
 		umatrix<T, Rows> result(r.columns());
-		for (size_t ri = 0; ri < result.rows(); ri++) {
-			for (size_t ci = 0; ci < result.columns(); ci++) {
-				T num = 0;
-				for (size_t i = 0; i < l.columns(); i++)
-					num += l(ri, i) * r(i, ci);
-				result(ri, ci) = num;
-			}
-		}
+		result.foreach([&](auto& val, auto ri, auto ci) { 
+			T num = T(0);
+			for (size_t i = 0; i < l.columns(); i++)
+				num += l(ri, i) * r(i, ci);
+			val = num;
+		});
 		return result;
 	}
 }
--- engine/include/engine/math/vector2D.hpp
+++ engine/include/engine/math/vector2D.hpp
@@ -10,13 +10,13 @@ namespace math {
 		~uvector2D() = default;
 		uvector2D<T>& operator+=(const uvector2D<T>& r) { x += r.x; y += r.y; return *this; }
 		uvector2D<T>& operator-=(const uvector2D<T>& r) { x -= r.x; y -= r.y; return *this; }
-		uvector2D<T>& operator*=(const uscalar<T>& r) { x *= r; y *= r; return *this; }
-		uvector2D<T>& operator/=(const uscalar<T>& r) { x /= r; y /= r; return *this; }
+		uvector2D<T>& operator*=(uscalar<T> r) { x *= r; y *= r; return *this; }
+		uvector2D<T>& operator/=(uscalar<T> r) { x /= r; y /= r; return *this; }
 	};
 	template<class T> uvector2D<T> operator + (const uvector2D<T>& l, const uvector2D<T>& r) { return { l.x + r.x, l.y + r.y }; };
 	template<class T> uvector2D<T> operator - (const uvector2D<T>& l, const uvector2D<T>& r) { return { l.x - r.x, l.y - r.y }; };
-	template<class T> uvector2D<T> operator * (const uscalar<T>& s, const uvector2D<T>& v) { return { s*v.x, s*v.y }; };
-	template<class T> uvector2D<T> operator * (const uvector2D<T>& v, const uscalar<T>& s) { return { v.x*s, v.y*s }; };
-	template<class T> uvector2D<T> operator / (const uscalar<T>& s, const uvector2D<T>& v) { return { s/v.x, s/v.y }; };
-	template<class T> uvector2D<T> operator / (const uvector2D<T>& v, const uscalar<T>& s) { return { v.x/s, v.y/s }; };
+	template<class T> uvector2D<T> operator * (uscalar<T> s, const uvector2D<T>& v) { return { s*v.x, s*v.y }; };
+	template<class T> uvector2D<T> operator * (const uvector2D<T>& v, uscalar<T> s) { return { v.x*s, v.y*s }; };
+	template<class T> uvector2D<T> operator / (uscalar<T> s, const uvector2D<T>& v) { return { s/v.x, s/v.y }; };
+	template<class T> uvector2D<T> operator / (const uvector2D<T>& v, uscalar<T> s) { return { v.x/s, v.y/s }; };
 }

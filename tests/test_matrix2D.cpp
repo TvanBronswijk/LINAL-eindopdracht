db1a29500f69f69450f72653a6a777a5c5130cbc
Rick van Berlo <rickvanberlo@gmail.com>
2019-01-12 20:58:53 +0100

    ctests for matrix 2D/3D

--- tests/test_matrix2D.cpp
+++ tests/test_matrix2D.cpp
@@ -16,22 +16,224 @@ int make() {
 	}
 }
 
+int plus() {
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f }
+		} };
+
+	math::umatrix2D<float> matrix_2 = matrix_1 + matrix_1;
+
+	if (matrix_2.at(2, 2) != 2.0f || matrix_2.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int minus() {
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f }
+		} };
+
+	math::umatrix2D<float> matrix_2 = matrix_1 - matrix_1;
+
+	if (matrix_2.at(2, 2) != 0.0f || matrix_2.at(0, 0) != 0.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int matrix_times_scalar() {
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f }
+		} };
+
+	math::umatrix2D<float> matrix_2 = matrix_1 * math::uscalar<float>(2.0f);
+
+	if (matrix_2.at(2, 2) != 2.0f || matrix_2.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int scalar_times_matrix() {
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f }
+		} };
+
+	math::umatrix2D<float> matrix_2 = math::uscalar<float>(2.0f) * matrix_1;
+
+	if (matrix_2.at(2, 2) != 2.0f || matrix_2.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int matrix_times_matrix() {//TODO::check
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f }
+		} };
+
+	math::umatrix2D<float> matrix_2 = matrix_1 * matrix_1;
+
+	if (matrix_2.at(2, 2) != 3.0f || matrix_2.at(0, 0) != 3.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int divide_by_scalar() {
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+		{ 2.0f, 2.0f, 2.0f },
+		{ 2.0f, 2.0f, 2.0f },
+		{ 2.0f, 2.0f, 2.0f }
+		} };
+
+	math::umatrix2D<float> matrix_2 = matrix_1 /= math::uscalar<float>(2.0f);
+
+	if (matrix_2.at(2, 2) != 1.0f || matrix_2.at(0, 0) != 1.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int plus_is() {
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f }
+		} };
+
+	matrix_1 += matrix_1;
+
+	if (matrix_1.at(2, 2) != 2.0f || matrix_1.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int minus_is() {
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f }
+		} };
+
+	matrix_1 -= matrix_1;
+
+	if (matrix_1.at(2, 2) != 0.0f || matrix_1.at(0, 0) != 0.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int times_is() {
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f }
+		} };
+
+	matrix_1 *= math::uscalar<float>(2.0f);
+
+	if (matrix_1.at(2, 2) != 2.0f || matrix_1.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int divide_by_is() {
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+		{ 2.0f, 2.0f, 2.0f },
+		{ 2.0f, 2.0f, 2.0f },
+		{ 2.0f, 2.0f, 2.0f }
+		} };
+
+	matrix_1 /= math::uscalar<float>(2.0f);
+
+	if (matrix_1.at(2, 2) != 1.0f || matrix_1.at(0, 0) != 1.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int matrix_operator() {
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+	{ 2.0f, 2.0f, 2.0f },
+	{ 2.0f, 2.0f, 2.0f },
+	{ 2.0f, 2.0f, 2.0f }
+	} };
+
+	if (matrix_1(2, 2) != 2.0f || matrix_1(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int foreach() {
+	math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
+	{ 1.0f, 1.0f, 1.0f },
+	{ 1.0f, 1.0f, 1.0f },
+	{ 1.0f, 1.0f, 1.0f }
+	} };
+
+	//matrix_1.foreach(plus_one);
+
+	if (matrix_1.at(2, 2) != 2.0f || matrix_1.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
 int main() {
 	int number = 0;
 	try {
 		number += make();
-		//number += plus();
-		//number += minus();
-		//number += matrix_times_scalar();
-		//number += scalar_times_matrix();
-		//number += matrix_times_matrix();
-		//number += divide_by_scalar();
-		//number += plus_is();
-		//number += minus_is();
-		//number += times_is();
-		//number += divide_by_is();
-		//number += matrix_operator();
-		//number += foreach();
+		number += plus();
+		number += minus();
+		number += matrix_times_scalar();
+		number += scalar_times_matrix();
+		number += matrix_times_matrix();
+		number += divide_by_scalar();
+		number += plus_is();
+		number += minus_is();
+		number += times_is();
+		number += divide_by_is();
+		number += matrix_operator();
+		number += foreach();
 
 
 		return number;
--- tests/test_matrix3D.cpp
+++ tests/test_matrix3D.cpp
@@ -1,39 +1,252 @@
-//#include <engine/math/transform3D.hpp>
-//#include <engine/math/matrix3D.hpp>
+#include <engine/math/transform3D.hpp>
+#include <engine/math/matrix3D.hpp>
 
 int make() {
-	//math::umatrix2D<float> matrix_1 = math::umatrix2D<float>::multidimensional_constructor<3>{ {
-	//	{ 1.0f, 1.0f, 1.0f },
-	//	{ 1.0f, 1.0f, 1.0f },
-	//	{ 1.0f, 1.0f, 1.0f }
-	//	} };
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+		} };
 
-	//if (matrix_1.at(1,1) != 1.0f || matrix_1.at(2,2) != 1.0f) {
-	//	return 0;
-	//}
-	//else {
-	//	return 1;
-	//}
-	return 1;
+	if (matrix_1.at(1, 1) != 1.0f || matrix_1.at(2, 2) != 1.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int plus() {
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+		} };
+
+	math::umatrix3D<float> matrix_2 = matrix_1 + matrix_1;
+
+	if (matrix_2.at(2, 2) != 2.0f || matrix_2.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int minus() {
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+		} };
+
+	math::umatrix3D<float> matrix_2 = matrix_1 - matrix_1;
+
+	if (matrix_2.at(2, 2) != 0.0f || matrix_2.at(0, 0) != 0.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int matrix_times_scalar() {
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+		} };
+
+	math::umatrix3D<float> matrix_2 = matrix_1 * math::uscalar<float>(2.0f);
+
+	if (matrix_2.at(2, 2) != 2.0f || matrix_2.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int scalar_times_matrix() {
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+		} };
+
+	math::umatrix3D<float> matrix_2 = math::uscalar<float>(2.0f) * matrix_1;
+
+	if (matrix_2.at(2, 2) != 2.0f || matrix_2.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int matrix_times_matrix() {//TODO::check
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+		} };
+
+	math::umatrix3D<float> matrix_2 = matrix_1 * matrix_1;
+
+	if (matrix_2.at(2, 2) != 4.0f || matrix_2.at(0, 0) != 4.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int divide_by_scalar() {
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+		{ 2.0f, 2.0f, 2.0f, 2.0f },
+		{ 2.0f, 2.0f, 2.0f, 2.0f },
+		{ 2.0f, 2.0f, 2.0f, 2.0f },
+		{ 2.0f, 2.0f, 2.0f, 2.0f }
+		} };
+
+	math::umatrix3D<float> matrix_2 = matrix_1 /= math::uscalar<float>(2.0f);
+
+	if (matrix_2.at(2, 2) != 1.0f || matrix_2.at(0, 0) != 1.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int plus_is() {
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+		} };
+
+	matrix_1 += matrix_1;
+
+	if (matrix_1.at(2, 2) != 2.0f || matrix_1.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int minus_is() {
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+	{ 1.0f, 1.0f, 1.0f, 1.0f },
+	{ 1.0f, 1.0f, 1.0f, 1.0f },
+	{ 1.0f, 1.0f, 1.0f, 1.0f },
+	{ 1.0f, 1.0f, 1.0f, 1.0f }
+	} };
+
+	matrix_1 -= matrix_1;
+
+	if (matrix_1.at(2, 2) != 0.0f || matrix_1.at(0, 0) != 0.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int times_is() {
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+		} };
+
+	matrix_1 *= math::uscalar<float>(2.0f);
+
+	if (matrix_1.at(2, 2) != 2.0f || matrix_1.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int divide_by_is() {
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+		{ 2.0f, 2.0f, 2.0f, 2.0f },
+		{ 2.0f, 2.0f, 2.0f, 2.0f },
+		{ 2.0f, 2.0f, 2.0f, 2.0f },
+		{ 2.0f, 2.0f, 2.0f, 2.0f }
+		} };
+
+	matrix_1 /= math::uscalar<float>(2.0f);
+
+	if (matrix_1.at(2, 2) != 1.0f || matrix_1.at(0, 0) != 1.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int matrix_operator() {
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+			{ 2.0f, 2.0f, 2.0f, 2.0f },
+			{ 2.0f, 2.0f, 2.0f, 2.0f },
+			{ 2.0f, 2.0f, 2.0f, 2.0f },
+			{ 2.0f, 2.0f, 2.0f, 2.0f }
+			} };
+
+	if (matrix_1(2, 2) != 2.0f || matrix_1(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
+int foreach() {
+	math::umatrix3D<float> matrix_1 = math::umatrix3D<float>::multidimensional_constructor<4>{ {
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f },
+		{ 1.0f, 1.0f, 1.0f, 1.0f }
+		} };
+
+	//matrix_1.foreach(plus_one);
+
+	if (matrix_1.at(2, 2) != 2.0f || matrix_1.at(0, 0) != 2.0f) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
 }
 
 int main() {
 	int number = 0;
 	try {
 		number += make();
-		//number += plus();
-		//number += minus();
-		//number += matrix_times_scalar();
-		//number += scalar_times_matrix();
-		//number += matrix_times_matrix();
-		//number += divide_by_scalar();
-		//number += plus_is();
-		//number += minus_is();
-		//number += times_is();
-		//number += divide_by_is();
-		//number += matrix_operator();
-		//number += foreach();
-
+		number += plus();
+		number += minus();
+		number += matrix_times_scalar();
+		number += scalar_times_matrix();
+		number += matrix_times_matrix();
+		number += divide_by_scalar();
+		number += plus_is();
+		number += minus_is();
+		number += times_is();
+		number += divide_by_is();
+		number += matrix_operator();
+		number += foreach();
 
 		return number;
 	}
